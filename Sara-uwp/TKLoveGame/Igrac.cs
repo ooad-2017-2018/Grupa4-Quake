f8988bc38dc1fcde0c51a82c8655688668ada42d
SaraHalilovic <shalilovic3@etf.unsa.ba>
2018-05-08 12:22:37 +0200

    Sara:klase+ pocetak validacije

---
+++ Sara-uwp/TKLoveGame/Igrac.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKLoveGame
+{
+    public abstract class Igrac//: Placanje
+    {
+        private String ime;
+        private String prezime;
+        private DateTime datum_rodjenja;
+        private String idIgraca;
+        private String password;
+        private String username;
+        private String email;
+        private int br_rundi;
+        private static int counter_igraci=1;
+
+        public Igrac(String ime, String prezime, String username, String password, String email, DateTime datumRodjenja)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Datum_rodjenja = datumRodjenja;
+            IdIgraca = Ime[0]+Prezime[0]+counter_igraci.ToString();
+            counter_igraci++;
+            Username = username;
+            Password = password;
+            br_rundi = 0;
+        }
+
+        public String Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        public String Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        public DateTime Datum_rodjenja
+        {
+            get { return datum_rodjenja; }
+            set { datum_rodjenja = value; }
+        }
+
+        public String IdIgraca
+        {
+            get { return idIgraca; }
+            set { idIgraca = value; }
+        }
+
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+
+    }
+
+
+    // void PlatiE() { }
+}
---
+++ Sara-uwp/TKLoveGame/Profesionalni.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKLoveGame
+{
+    public class Profesionalni:Igrac
+    {
+        private int rank;
+        private int bodovi;
+        private String best_result;
+        //private List<Takmicenje> MyTakmicenje = new List<Takmicenje>();
+
+        public Profesionalni(String ime, String prezime, String username, String password, String email, DateTime datumRodjenja) : base(ime, prezime, username, password, email, datumRodjenja) { bodovi = 0; best_result = "/"; rank = 0; }
+
+        public int Rank
+        {
+            get { return rank; }
+            set { rank = value; }
+
+        }
+
+
+        public int Bodovi
+        {
+            get { return bodovi; }
+            set { bodovi = value; }
+        }
+
+        public String BestResult
+        {
+            get { return best_result; }
+            set { best_result = value; }
+        }
+    }
+}
