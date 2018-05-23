9f0a09ebc3d5ba3704b2d3931132a105e233aa32
aminaanaan <amina-mo1@hotmail.com>
2018-05-23 17:26:10 +0200

    commit tklovegame klase

--- uwp/TKLoveGame/Mec.cs
+++ uwp/TKLoveGame/Mec.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Windows.System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- uwp/TKLoveGame/TKLoveGame.cs
+++ uwp/TKLoveGame/TKLoveGame.cs
@@ -12,10 +12,10 @@ namespace TKLoveGame
         private List<Zaposlenik> listaZaposlenika;
         private List<Igrac> listaIgraca;
         private List<Profesionalni> RangLista;
-       /* private List<SaraSlam> SaraSlam;
+        private List<SaraSlam> SaraSlam;
         private List<Teren> listaTerena;
         private List<Takmicenje> listaTakmicenja;
-        private List<Rezervacija>listaRezervacija;*/
+        private List<Rezervacija>listaRezervacija;
 
        
         public TKLoveGame()
@@ -23,11 +23,11 @@ namespace TKLoveGame
             listaZaposlenika = new List<Zaposlenik>();
             listaIgraca = new List<Igrac>();
             RangLista = new List<Profesionalni>();
-            /*
+           
             SaraSlam = new List<SaraSlam>();
             listaTerena = new List<Teren>();
             listaRezervacija = new List<Rezervacija>();
-            */
+           
 
         }
 
@@ -48,6 +48,9 @@ namespace TKLoveGame
             get { return RangLista; }
         }
 
-
+        public List<SaraSlam> SaraSlam1 { get => SaraSlam; set => SaraSlam = value; }
+        public List<Teren> ListaTerena { get => listaTerena; set => listaTerena = value; }
+        public List<Takmicenje> ListaTakmicenja { get => listaTakmicenja; set => listaTakmicenja = value; }
+        public List<Rezervacija> ListaRezervacija { get => listaRezervacija; set => listaRezervacija = value; }
     }
 }
